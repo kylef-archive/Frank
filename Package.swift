354ad0d7579eed79ac19568bd75f8e74cec449d3
Kyle Fuller <kyle@fuller.li>
2016-03-05 18:37:45 +0000

    Update Curassow to 0.4

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
     .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 4),
   ],
   testDependencies: [
     .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
